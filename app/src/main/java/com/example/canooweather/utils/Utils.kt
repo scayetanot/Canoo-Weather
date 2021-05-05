387d2a05a252cc412fa95e40c92c06044dca0055
Sebastien Cayetanot <sebastien.cayetanot@gmail.com>
2021-05-05 00:04:59 -0700

    Update date

--- app/src/main/java/com/example/canooweather/utils/Utils.kt
+++ app/src/main/java/com/example/canooweather/utils/Utils.kt
@@ -3,12 +3,22 @@ package com.example.canooweather.utils
 import android.net.Uri
 import android.text.format.DateFormat
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 
-fun convertToReadableDate(timestamp: Long): String =
-    SimpleDateFormat("EEE", Locale.ENGLISH).format(timestamp)
-        .toUpperCase(Locale.ENGLISH)
+fun convertToReadableDate(timestamp: Long): String {
+
+    val fullDayFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+
+    val timestampAsDateString = DateTimeFormatter.ISO_INSTANT
+        .format(java.time.Instant.ofEpochSecond(timestamp))
+
+    val date = LocalDate.parse(timestampAsDateString, fullDayFormat)
+
+    return date.dayOfWeek.toString().substring(0,3)
+}
 
 fun convertToReadableHours(timestamp: Long): String {
     val cal = Calendar.getInstance(Locale.ENGLISH)
